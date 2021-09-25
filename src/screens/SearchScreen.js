d34b670d2045f7ec8a4ef4438f092a19e7fab792
Daniel <itzdanielmichael@gmail.com>
2021-09-24 22:00:09 -0400

    create RestaurantsList.js

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import SearchBar from '../components/SearchBar';
+import React, { useState } from 'react';
 import useRestaurants from '../hooks/useRestaurants';
+
+import { View, Text, StyleSheet } from 'react-native';
+import RestaurantsList from '../components/RestaurantsList';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -17,6 +19,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <RestaurantsList title="Cost Effective" />
+      <RestaurantsList title="Bit Pricier" />
+      <RestaurantsList title="Big Spender" />
     </View>
   );
 };
