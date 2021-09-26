30209afa97b6a397881f874f544effe3a1158ed7
Daniel <itzdanielmichael@gmail.com>
2021-09-25 21:20:15 -0400

    update RestaurantsShowScreen.js

--- src/screens/RestaurantsShowScreen.js
+++ src/screens/RestaurantsShowScreen.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelpApi from '../services/yelpApi';
+
+const getRestaurant = async (id) => {
+  const response = await yelpApi.get(`/${id}`);
+  return response.data;
+};
 
 const RestaurantsShowScreen = ({ navigation }) => {
+  const [restaurant, setRestaurant] = useState(null);
   const id = navigation.getParam('id'); // accessing id that was passed to this component with navigation.navigate
+
+  useEffect(() => {
+    const fetchRestaurantOnMount = async () => {
+      const oneRestaurant = await getRestaurant(id);
+      setRestaurant(oneRestaurant);
+    };
+
+    fetchRestaurantOnMount();
+  }, []);
 
   return (
     <View>
