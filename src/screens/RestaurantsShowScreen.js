3287e643d3ade38eede7ef32274f76f11999246b
Daniel <itzdanielmichael@gmail.com>
2021-09-25 21:26:34 -0400

    update RestaurantsShowScreen.js

--- src/screens/RestaurantsShowScreen.js
+++ src/screens/RestaurantsShowScreen.js
@@ -1,32 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import yelpApi from '../services/yelpApi';
 
-const getRestaurant = async (id) => {
+const getOneRestaurant = async (id) => {
   const response = await yelpApi.get(`/${id}`);
   return response.data;
 };
 
+// a screen that shows data regarding ONE restaurant.
 const RestaurantsShowScreen = ({ navigation }) => {
   const [restaurant, setRestaurant] = useState(null);
   const id = navigation.getParam('id'); // accessing id that was passed to this component with navigation.navigate
 
   useEffect(() => {
     const fetchRestaurantOnMount = async () => {
-      const oneRestaurant = await getRestaurant(id);
+      const oneRestaurant = await getOneRestaurant(id);
       setRestaurant(oneRestaurant);
     };
 
     fetchRestaurantOnMount();
   }, []);
 
+  if (!restaurant) return null;
+
   return (
     <View>
-      <Text>Results Show</Text>
+      <Text>{restaurant.name}</Text>
+
+      <FlatList
+        data={restaurant.photos}
+        keyExtractor={(photo) => photo} // all photos are unique strings, so theyre safe to use as keys
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
 
 export default RestaurantsShowScreen;
